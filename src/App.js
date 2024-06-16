54c184903a2b42038c115762c8cc3baf4428804a
Max <ring.maks@gmail.com>
2024-06-16 15:28:47 +0300

    added search page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Box, FormControl, IconButton, Input, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box sx={{
+                bgcolor: '#FFF',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                p: 3,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'}}>
+                <FormControl sx={{m: 1, width: '500px'}} variant="outlined">
+                    <OutlinedInput
+                        id='film-name'
+                        type='text'
+                        placeholder='Input film or serial'
+                        fullWidth={true}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton>
+                                    <SearchIcon/>
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
