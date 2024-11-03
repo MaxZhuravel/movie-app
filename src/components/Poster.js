50841011907040473dd9b514f8ad780b3db8eaf6
Max <ring.maks@gmail.com>
2024-11-03 09:51:45 +0200

    shadow for poster

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Box} from "@mui/material";
 
 const Poster = ({path}) => {
     return (
-        <img src={path} style={{width: '100%'}}/>
+        <Box sx={{" img": {boxShadow: 10}}}>
+            <img src={path} style={{width: '100%'}} alt="Poster image"/>
+        </Box>
     );
 };
 
